4c7e73dcfc1d905b8e74186d2b536bbaab39c27e
unknown <skafosbiz@gmail.com>
2021-03-21 16:41:30 +0200

    make it better

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
-const devMode = process.env.NODE_ENV !== "production";
+
 
 module.exports = {
   entry: ["babel-polyfill", "./src/index"],
